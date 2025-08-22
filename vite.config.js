535e269b3547c311e9489751ddc5791234365c32
Blake Rears <125698548+BlakeRears@users.noreply.github.com>
2025-08-22 14:07:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/blakes-react-website/",
+  base: "/",
   plugins: [tailwindcss(), react()],
 })
