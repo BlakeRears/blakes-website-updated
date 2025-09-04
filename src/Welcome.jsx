fcef4e080db3dcf6eeddbbc91c5c019669f99a10
BlakeRears <125698548+BlakeRears@users.noreply.github.com>
2025-09-04 18:50:13 -0400

    git commit -m"Fixed pfp loading on phone"

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import React, { useState } from 'react';
 import pfp from '/public/HeadshotPFPCircle.png';
-import pfpanimation from './assets/headshotanimationweb.webm';
-import pfpanimationiphone from './assets/headshotanimation.mov';
+import pfpanimation from './assets/output.webm';
 
 function Welcome() {
 
@@ -34,7 +33,7 @@ function Welcome() {
                 <div className="">
                     <h2 id='topPage' className="text-4xl md:text-5xl lg:text-6xl xl:text-[110px]">Hello I'm</h2>
                     <h2 className="text-5xl md:text-7xl lg:text-8xl xl:text-[130px] font-bold">Blake Rears</h2>
-                    <h2 className="text-2xl md:text-4xl lg:text-5xl xl:text-[50px] font-mono text-cyan-200 my-2 md:mx-1">{displayedText}</h2>
+                    <h2 className="text-2xl md:text-4xl lg:text-5xl xl:text-[50px] font-mono text-cyan-200 my-2">{displayedText}</h2>
                     <a
                         href="#projects"
                         className="hidden text-4xl bg-transparent border-2 rounded-[50px] hover:bg-gray-200 text-cyan-500 font-semibold p-5 shadow-lg transition-colors duration-300 xl:inline-block mt-10 text-shadow-mg"
@@ -43,10 +42,9 @@ function Welcome() {
                     </a>
                 </div>
 
-                <div className="relative overflow-hidden rounded-full p-3 w-[300px] h-[300px] xl:w-[500px] xl:h-[500px]">
+                <div className="relative overflow-hidden rounded-full w-[300px] h-[300px] xl:w-[500px] xl:h-[500px]">
                     <video
                         autoPlay
-                        loop
                         muted
                         playsInline
                         className="w-full h-full object-cover"
